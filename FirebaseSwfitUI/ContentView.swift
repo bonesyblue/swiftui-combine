68f0586226fdbaec83d57f8340c5aa66c03a40ac
bonesyblue <bonesyblue@gmail.com>
2020-07-09 18:07:58 +0200

    chore: Add boilerplate SwiftUI project

---
+++ FirebaseSwfitUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirebaseSwfitUI
+//
+//  Created by Jonathan Bones on 09.07.20.
+//  Copyright © 2020 bonesyblue. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
